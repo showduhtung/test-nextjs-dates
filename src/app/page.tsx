93267d6b74dc5898c722d4514ecd6aa54d9fe05b
Shaun Tung <shaun.tung@aicadium.ai>
2024-09-27 20:58:09 +0800

    push beter names and package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { TestDate, TextTZDate, TZDateClient } from "~/testers";
 export default function Statically() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1 className="text-4xl font-bold">Dynamically Rendered</h1>
+      <h1 className="text-4xl font-bold">Statically Rendered</h1>
       <div className="flex gap-20">
         <TestDate />
         <TextTZDate />
