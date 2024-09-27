b6081e65f98dcbc64efe02599c0475ebddfabb8d
Shaun Tung <shaun.tung@aicadium.ai>
2024-09-27 20:13:09 +0800

    update slots

--- src/app/ppr/@force_dynamic/page.tsx
+++ src/app/ppr/@force_dynamic/page.tsx
@@ -1,6 +1,7 @@
 import { TestDate, TextTZDate, TZDateClient } from "~/testers";
 
 export const dynamic = "force-dynamic";
+
 export default function ForceDynamic() {
   return (
     <div className="flex gap-20">
