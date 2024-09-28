7d8dfe0d9698ad69caf4ad093fc47a3af2f8c9f2
Shaun Tung <shaun.tung@aicadium.ai>
2024-09-28 23:41:19 +0800

    update hourly selected hour page

--- src/app/hourly/[selectedHour]/page.tsx
+++ src/app/hourly/[selectedHour]/page.tsx
@@ -1,5 +1,48 @@
-type HourPageProps = Readonly<{ params: unknown; searchParams: unknown }>;
+import Link from "next/link";
+import { PATTERN } from "~/common";
+import { PageParams, parseParams } from "~/common/utilities";
+import { Button } from "~/components/button";
+import { Time } from "~/components/time";
 
-export default function HourPage({ params, searchParams }: HourPageProps) {
-  return <pre>{JSON.stringify({ params, searchParams }, null, 2)}</pre>;
+type HourlyParams = Readonly<{ selectedHour: Date }>;
+type PageProps = { params: PageParams<HourlyParams> };
+
+export default function HourPage({ params }: PageProps) {
+  const { selectedHour } = parseParams<HourlyParams>(params);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Button className="w-fit" asChild>
+        <Link href="/hourly">Back</Link>
+      </Button>
+      <div className="flex flex-col gap-1">
+        <strong className="text-2xl">URL State:</strong>
+        <div className="flex items-center justify-between text-primary">
+          <dfn className="text-xs">
+            <code>&quot;selectedHour.toUTCString()&quot;</code>
+          </dfn>
+
+          <time>{selectedHour.toUTCString()}</time>
+        </div>
+
+        <div className="flex items-center justify-between text-primary">
+          <dfn className="text-xs">
+            <code>&quot;selectedHour.toISOString()&quot;</code>
+          </dfn>
+
+          <time>{selectedHour.toISOString()}</time>
+        </div>
+      </div>
+      <div className="flex flex-col gap-1">
+        <strong className="text-2xl">UI Display:</strong>
+        <div className="flex items-center justify-between text-primary">
+          <dfn className="text-xs">
+            <code>&quot;&lt;Time&gt;&#123;selectedHour.toISOString()&#125;&lt;/Time&gt;&quot;</code>
+          </dfn>
+
+          <Time pattern={PATTERN}>{selectedHour.toISOString()}</Time>
+        </div>
+      </div>
+    </div>
+  );
 }
