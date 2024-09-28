c95dff5ab224e52b469047bc71e4181c61e85ff1
Shaun Tung <shaun.tung@aicadium.ai>
2024-09-28 23:44:38 +0800

    push hourly page proofs

--- src/app/hourly/page.tsx
+++ src/app/hourly/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { PATTERN } from "~/common";
 import { Button } from "~/components/button";
 import { Time } from "~/components/time";
 import {
@@ -41,31 +42,79 @@ export default function HourlyBlocks() {
   });
 
   return (
-    <div className="flex flex-col gap-8">
-      {blocks.map(({ today, hours }) => (
-        <div key={today.getTime()} className="flex flex-col gap-4">
-          <h6 className="text-xl font-semibold text-primary">{format(today, "eee, d MMM yyyy")}</h6>
-          <div className="flex flex-wrap gap-4 pl-1">
-            {hours.map(({ timestamp }) => {
-              return (
-                <Button asChild key={timestamp.toISOString()} className="w-fit">
-                  <Link
-                    className="button-press-effect flex w-24 items-center justify-between bg-blue-50 p-2"
-                    href={`/hourly/${timestamp.toISOString()}`}
-                  >
-                    <Time
-                      className="whitespace-nowrap text-nowrap text-lg font-semibold uppercase"
-                      pattern="hh:mm a"
-                    >
-                      {timestamp.toISOString()}
-                    </Time>
-                  </Link>
-                </Button>
-              );
-            })}
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-1">
+        <strong className="text-2xl">Boundaries:</strong>
+        <div className="flex items-center justify-between text-primary">
+          <dfn className="text-xs">
+            <code>&quot;boundaryStart.toUTCString()&quot;</code>
+          </dfn>
+
+          <div className="flex w-[550px] justify-between gap-4">
+            <time>{boundaryStart.toUTCString()}</time>
+            <p>|</p>
+            <time>{boundaryEnd.toUTCString()}</time>
           </div>
         </div>
-      ))}
+
+        <div className="flex items-center justify-between text-primary">
+          <dfn className="text-xs">
+            <code>&quot;boundaryStart.toISOString()&quot;</code>
+          </dfn>
+
+          <div className="flex w-[550px] justify-between gap-4">
+            <time>{boundaryStart.toISOString()}</time>
+            <p>|</p>
+            <time>{boundaryEnd.toISOString()}</time>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <strong className="text-2xl">UI Display:</strong>
+          <div className="flex items-center justify-between text-primary">
+            <dfn className="text-xs">
+              <code>
+                &quot;&lt;Time&gt;&#123;boundaryStart.toISOString()&#125;&lt;/Time&gt;&quot;
+              </code>
+            </dfn>
+
+            <div className="flex w-[550px] justify-between gap-4">
+              <Time pattern={PATTERN}>{boundaryStart.toISOString()}</Time>
+              <p>|</p>
+              <Time pattern={PATTERN}>{boundaryEnd.toISOString()}</Time>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-8">
+        {blocks.map(({ today, hours }) => (
+          <div key={today.getTime()} className="flex flex-col gap-4">
+            <h6 className="text-xl font-semibold text-primary">
+              {format(today, "eee, d MMM yyyy")}
+            </h6>
+            <div className="flex flex-wrap gap-4 pl-1">
+              {hours.map(({ timestamp }) => {
+                return (
+                  <Button asChild key={timestamp.toISOString()} className="w-fit">
+                    <Link
+                      className="button-press-effect flex w-24 items-center justify-between bg-blue-50 p-2"
+                      href={`/hourly/${timestamp.toISOString()}`}
+                    >
+                      <Time
+                        className="whitespace-nowrap text-nowrap text-lg font-semibold uppercase"
+                        pattern="hh:mm a"
+                      >
+                        {timestamp.toISOString()}
+                      </Time>
+                    </Link>
+                  </Button>
+                );
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
