c38ca684ba6f3f0973a42ca375fcf5495e5e8b15
Shaun Tung <shaun.tung@aicadium.ai>
2024-10-01 01:54:05 +0800

    update raw hourly blocks

--- src/app/hourly/@raw_date_fns/page.tsx
+++ src/app/hourly/@raw_date_fns/page.tsx
@@ -14,8 +14,9 @@ import {
   getHours,
 } from "date-fns";
 
-import { mockFetchBoundaries } from "~/data";
+import { mockFetchLocalizedBoundaries } from "~/data";
 import { HourlyBlocks } from "../_components/hourly-block";
+import { TZDate } from "~/libs/date-fns";
 
 const functions = {
   addDays,
@@ -32,9 +33,17 @@ const functions = {
 } as typeof DateFns;
 
 export default async function RawDateFnsPage() {
-  const [boundaryStart, boundaryEnd = new Date()] = await mockFetchBoundaries(
-    new Date("Sep 14 2024, 2:00:00 PM GMT+0"),
+  const [boundaryStart, boundaryEnd = TZDate.tz()] = await mockFetchLocalizedBoundaries(
+    new TZDate("Sep 14 2024, 2:00:00 PM GMT+0"),
   );
 
-  return <HourlyBlocks boundaries={[boundaryStart, boundaryEnd]} functions={functions} />;
+  return (
+    <>
+      <HourlyBlocks boundaries={[boundaryStart, boundaryEnd]} functions={functions} />
+      <HourlyBlocks
+        boundaries={[new TZDate(boundaryStart), new TZDate(boundaryEnd)]}
+        functions={functions}
+      />
+    </>
+  );
 }
