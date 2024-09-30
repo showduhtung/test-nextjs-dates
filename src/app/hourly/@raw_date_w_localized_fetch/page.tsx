988b827ec09412308b91a548d6e9536c8cc54332
Shaun Tung <shaun.tung@aicadium.ai>
2024-10-01 02:09:32 +0800

    localized w fetch hourly blocks

--- src/app/hourly/@raw_date_w_localized_fetch/page.tsx
+++ src/app/hourly/@raw_date_w_localized_fetch/page.tsx
@@ -14,8 +14,9 @@ import {
   getHours,
 } from "date-fns";
 
-import { mockFetchBoundaries } from "~/data";
+import { mockFetchLocalizedBoundaries } from "~/data";
 import { HourlyBlocks } from "../_components/hourly-block";
+import { TZDate } from "~/libs/date-fns";
 
 const functions = {
   addDays,
@@ -31,10 +32,15 @@ const functions = {
   addHours,
 } as typeof DateFns;
 
-export default async function RawDateFnsPage() {
-  const [boundaryStart, boundaryEnd = new Date()] = await mockFetchBoundaries(
-    new Date("Sep 14 2024, 2:00:00 PM GMT+0"),
+export default async function RawDateFnWLocalizedFetchPage() {
+  const [boundaryStart, boundaryEnd = TZDate.tz()] = await mockFetchLocalizedBoundaries(
+    new TZDate("Sep 14 2024, 2:00:00 PM GMT+0"),
   );
 
-  return <HourlyBlocks boundaries={[boundaryStart, boundaryEnd]} functions={functions} />;
+  return (
+    <HourlyBlocks
+      boundaries={[new TZDate(boundaryStart), new TZDate(boundaryEnd)]}
+      functions={functions}
+    />
+  );
 }
--- src/app/hourly/layout.tsx
+++ src/app/hourly/layout.tsx
@@ -4,6 +4,7 @@ type HourlyLayoutProps = {
   children: React.ReactNode;
   native: React.ReactNode;
   raw_date_fns: React.ReactNode;
+  raw_date_w_localized_fetch: React.ReactNode;
   localized_date_fns: React.ReactNode;
 };
 
@@ -11,6 +12,7 @@ export default function HourlyLayout({
   children,
   native,
   localized_date_fns,
+  raw_date_w_localized_fetch,
   raw_date_fns,
 }: HourlyLayoutProps) {
   return (
@@ -21,6 +23,9 @@ export default function HourlyLayout({
         <TabsList className="mb-8">
           <TabsTrigger value="native">Native JS</TabsTrigger>
           <TabsTrigger value="raw_date_fns">Raw date-fns</TabsTrigger>
+          <TabsTrigger value="raw_date_w_localized_fetch">
+            Raw date-fns w/ Localized Fetch
+          </TabsTrigger>
           <TabsTrigger value="localized_date_fns">Localized date-fns (Solution)</TabsTrigger>
         </TabsList>
         <TabsContent value="native" className="w-full">
@@ -28,6 +33,9 @@ export default function HourlyLayout({
         </TabsContent>
         <TabsContent value="raw_date_fns" className="w-full">
           {raw_date_fns}
+        </TabsContent>
+        <TabsContent value="raw_date_w_localized_fetch" className="w-full">
+          {raw_date_w_localized_fetch}
         </TabsContent>
         <TabsContent value="localized_date_fns" className="w-full">
           {localized_date_fns}
