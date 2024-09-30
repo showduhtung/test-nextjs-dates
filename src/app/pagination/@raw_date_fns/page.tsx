95724479a94f59a6e16b2541d535486714d42afe
Shaun Tung <shaun.tung@aicadium.ai>
2024-10-01 00:40:00 +0800

    added more select/pagination

--- src/app/pagination/@raw_date_fns/page.tsx
+++ src/app/pagination/@raw_date_fns/page.tsx
@@ -32,11 +32,9 @@ export default async function DateFnsPage({ searchParams }: PaginationPageProps)
   const { interval, selectedDates } = parseSearchParams<PaginationSearchParams>(searchParams);
   if (!interval || !selectedDates) return <>Missing params</>;
 
-  const [boundaryStart, boundaryEnd = new TZDate()] = (await mockFetchLocalizedBoundaries()) as [
-    TZDate,
-    TZDate,
-  ];
+  const [first, second = new Date()] = await mockFetchLocalizedBoundaries();
 
+  const [boundaryStart, boundaryEnd] = [new TZDate(first), new TZDate(second)];
   const [selectedStart, selectedEnd] = [new TZDate(selectedDates[0]), new TZDate(selectedDates[1])];
 
   return (
@@ -52,6 +50,19 @@ export default async function DateFnsPage({ searchParams }: PaginationPageProps)
         selectedDates={[selectedStart, selectedEnd]}
         functions={functions}
         boundaries={[boundaryStart, boundaryEnd]}
+      />
+
+      <IntervalSelect
+        interval={interval}
+        selectedDates={selectedDates}
+        functions={functions}
+        boundaries={[first, second]}
+      />
+      <IntervalPagination
+        interval={interval}
+        selectedDates={selectedDates}
+        functions={functions}
+        boundaries={[first, second]}
       />
     </div>
   );
