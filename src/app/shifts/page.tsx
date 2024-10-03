5b1a912e60812b0e6ffcec073888ebcd3473e16e
Shaun Tung <shaun.tung@aicadium.ai>
2024-10-03 15:15:15 +0800

    update shfits

--- src/app/shifts/page.tsx
+++ src/app/shifts/page.tsx
@@ -1,6 +1,12 @@
-import { getHours } from "~/libs/date-fns";
+import { Time } from "~/components/time";
+import { format, getHours } from "~/libs/date-fns";
 
 export default function Page() {
   const hour = getHours(new Date());
-  return <>Hour is : {hour}</>;
+  return (
+    <div>
+      <p>Hour is : {hour}</p>
+      <Time>This is the time : {format(new Date(), "dd MMM hh:mm a")}</Time>
+    </div>
+  );
 }
